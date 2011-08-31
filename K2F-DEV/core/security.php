dcf9dc879a1b6448539f78fae75336d6b2d96aaa
root <root@cov.covac-software.com>
2011-08-31 17:45:11 -0500

    1314830710 gitjob update

--- K2F-DEV/core/security.php
+++ K2F-DEV/core/security.php
@@ -5,7 +5,7 @@
 		// @site http://anantgarg.com/2010/02/18/cross-domain-cookies-in-safari/comment-page-1/#comment-5114
 		header('P3P: CP="IDC DSP COR CURa ADMa OUR IND PHY ONL COM STA"',true);
 		// Broadcast system software version.
-		header('X-Powered-By: K2F/'.K2F,true);
+		//header('X-Powered-By: K2F/'.K2F,true); // security issue
 		// Tell browsers this site should not be used in iframes on other pages.
 		header('X-Frame-Options: SAMEORIGIN',true);
 		// Tell browsers that if they think an XSS attack s going on, block it.
@@ -21,6 +21,7 @@
 	}else xlog('Warning: Could not send critical headers.');
 
 	// create hmac function for PHP 5.1 and older
+	// TODO http://chat.stackoverflow.com/transcript/message/1361148#1361148
 	if(!function_exists('hash_hmac')){
 		function hash_hmac($algo, $data, $key, $raw_output = false) {
 			$pack=array('sha1'=>'H40','md5'=>'H32');
@@ -47,29 +48,31 @@
 	 *          25/06/2011 - Fixed method `escape` to handle different data types better.
 	 *          25/06/2011 - Added method `escapeQuote` which escapes and adds double quotes to input. 
 	 *          16/08/2011 - Added method `removeHomoglyphs`, used to remove similarly-looking characters.
+	 *          31/08/2011 - Checked by Anthony Ferrara (http://chat.stackoverflow.com/transcript/message/1361128#1361128)
 	 */
 	class Security {
 		/**
-		 * Generate a Globally Unique Identifier
-		 * @return string The GUID in HEX
+		 * @return string Generates 16-byte random string.
+		 * @author Anthony Ferrara
+		 */
+		public static function genUID() {
+			$seed = '';
+			if (!function_exists('mcrypt_create_iv'))
+				for ($i = 0; $i < 16; $i++)
+					$seed .= chr(mt_rand(0, 255) ^ mt_rand(0, 255));
+			else
+				$seed = mcrypt_create_iv(16, MCRYPT_DEV_URANDOM);
+			$seed[6] = chr((ord($seed[6]) & 0x0F) | 0x40);
+			$seed[8] = chr((ord($seed[8]) & 0x3F) | 0x80);
+			return $seed;
+		}
+		/**
+		 * @return string A Globally Unique Identifier in GUID text-hex format (excl braces).
 		 */
 		public static function genGUID(){
-			// @fixme: http://www.suspekt.org/2008/08/17/mt_srand-and-not-so-random-numbers/
-			$workid=strtoupper(md5(uniqid(mt_rand(),true)));
-			$byte=hexdec(substr($workid,12,2));
-			$byte=$byte & hexdec("0f");
-			$byte=$byte | hexdec("40");
-			$workid=substr_replace($workid,strtoupper(dechex($byte)),12,2);
-			$byte=hexdec(substr($workid,16,2));
-			$byte=$byte & hexdec("3f");
-			$byte=$byte | hexdec("80");
-			$workid=substr_replace($workid,strtoupper(dechex($byte)),16,2);
-			$wid=substr($workid, 0, 8).'-'
-				.substr($workid, 8, 4).'-'
-				.substr($workid,12, 4).'-'
-				.substr($workid,16, 4).'-'
-				.substr($workid,20,12);
-			return $wid;
+			$uid_ar = array_map('ord', str_split(self::genUID()));
+			$format = '%02X%02X%02X%02X-%02X%02X-%02X%02X-%02X%02X-%02X%02X%02X%02X%02X%02X';
+			return vsprintf($format, $uid_ar);
 		}
 		/**
 		 * @var array Holds token constants for genToken function.
@@ -106,6 +109,7 @@
 		 * @param integer $limit The number of iterations possible.
 		 *     This is a safeguard against infinite looping.
 		 * @return string The generated random token.
+		 * @todo http://chat.stackoverflow.com/transcript/message/1361180#1361180
 		 */
 		public static function genToken($letters,$length,$nohomoglyphs=false,$limit=PHP_INT_MAX){
 			// split tokens and initialize variables
@@ -172,25 +176,27 @@
 		/**
 		 * Converts a string's HTML metacharacters into their representation.
 		 * @param string $data The original/insecure data.
+		 * @param string $charset (Optional) The target character set.
 		 * @return string The new data.
 		 */
-		public static function snohtml($data){
-			$data=htmlspecialchars($data,ENT_QUOTES,'ISO-8859-1');
+		public static function snohtml($data,$charset='ISO-8859-1'){
+			$data=htmlspecialchars($data,ENT_QUOTES,$charset);
 			return $data;
 		}
 		/**
 		 * Converts an array's values (and optionally, keys) into HTML-safe data.
 		 * @param array $array The original array to secure.
 		 * @param boolean $keys_too (Optional) set to false to disable securing keys. Default is "true".
+		 * @param string $charset (Optional) The target character set.
 		 * @return array The newly secured array.
 		 */
-		public static function anohtml(&$array,$keys_too=true){
+		public static function anohtml(&$array,$keys_too=true,$charset='ISO-8859-1'){
 			if(isset($array[self::$RECURSE_PROTECT]))return '[RECURSION]';				// recursion protect
 			$array[self::$RECURSE_PROTECT]='';											// recursion protect
 			$new=array();
 			foreach($array as $key=>$value)if($key!==self::$RECURSE_PROTECT){
 				$value=self::vnohtml($value);
-				if($keys_too)$key=self::snohtml($key);
+				if($keys_too)$key=self::snohtml($key,$charset);
 				$new[$key]=$value;
 			}
 			unset($array[self::$RECURSE_PROTECT]);										// recursion protect
@@ -200,15 +206,16 @@
 		 * Converts an object's property values (and optionally, property names/keys) into HTML-safe data.
 		 * @param object $array The original object to secure.
 		 * @param boolean $keys_too (Optional) set to false to disable securing keys. Default is "true".
+		 * @param string $charset (Optional) The target character set.
 		 * @return object The newly secured object.
 		 */
-		public static function onohtml($object,$keys_too=true){
+		public static function onohtml($object,$keys_too=true,$charset='ISO-8859-1'){
 			if(isset($object->{self::$RECURSE_PROTECT}))return '[RECURSION]';			// recursion protect
 			$object->{self::$RECURSE_PROTECT}='';										// recursion protect
 			$new=$object;
 			foreach(get_object_vars($object) as $key=>$value)if($key!==self::$RECURSE_PROTECT){
 				$value=self::vnohtml($value);
-				if($keys_too)$key=self::snohtml($key);
+				if($keys_too)$key=self::snohtml($key,$charset);
 				$new->{$key}=$value;
 			}
 			unset($object->{self::$RECURSE_PROTECT});									// recursion protect
@@ -217,16 +224,17 @@
 		/**
 		 * Secure a variable for HTML output (by removing metacharacters).
 		 * @param misc $var The original variable.
+		 * @param string $charset (Optional) The target character set.
 		 * @return misc The secured variable.
 		 */
-		public static function vnohtml($var){
-			if(is_array($var))	return self::anohtml($var);
+		public static function vnohtml($var,$charset='ISO-8859-1'){
+			if(is_array($var))	return self::anohtml($var,$charset);
 			if(is_bool($var))	return $var; // safe type
 			if(is_float($var))	return $var; // safe type
 			if(is_integer($var))return $var; // safe type
 			if(is_null($var))	return $var; // safe type
-			if(is_object($var))	return self::onohtml($var);
-			if(is_string($var))	return self::snohtml($var);
+			if(is_object($var))	return self::onohtml($var,$charset);
+			if(is_string($var))	return self::snohtml($var,$charset);
 			return null; // unknown variable type, assume unsafe.
 		}
 		/**
@@ -252,27 +260,9 @@
 			die;
 		}
 		/**
-		 * Replaces all characters except underscore, alphabet and numerals.
-		 * Usefull in eg; HTML id and name attributes, filenames etc...
-		 * @param string $orig The original string.
-		 * @param string $replace The replace string (defaults to a single underscore).
-		 * @return string The new string.
-		 */
-		public static function stoident($orig,$replace='_'){
-			$orig=(string)$orig; // input failsafe
-			for($i=0; $i<strlen($orig); $i++){
-				$o=ord($orig{$i});
-				if(!(  (($o>=48) && ($o<=57))			// numbers
-					|| (($o>=97) && ($o<=122))			// lowercase
-					|| (($o>=65) && ($o<=90))			// uppercase
-					|| ($orig{$i}=='_')))				// underscore
-					$orig{$i}=$replace;
-			}
-			return $orig;
-		}
-		/**
 		 * Escapes a value for use within a DB query.<br>
 		 * <b>WARNING</b> If the original value is of enumerable type, the index is NEVER escaped.
+		 * @deprecated WARNING DO NOT USE THE METHOD. Instead use Database::db()->escape().
 		 * @param mixed $val Original value.
 		 * @return array|string The escaped value.
 		 */
@@ -296,6 +286,7 @@
 		/**
 		 * Similar tp escape(), but adds double quotes when necesary.<br>
 		 * <b>WARNING</b> If the original value is of enumerable type, the index is NEVER escaped.
+		 * @deprecated WARNING DO NOT USE THE METHOD. Instead use Database::db()->escape().
 		 * @param mixed $val Original value.
 		 * @return array|string The escaped value.
 		 * @example Security::escapeQuote(array("Hi",45,true)); // array('"Hi"','45','1')
@@ -318,26 +309,27 @@
 			}
 		}
 		/**
+		 * Replaces all characters except underscore, alphabet and numerals.
+		 * Usefull in eg; HTML id and name attributes, filenames, variable names etc...
+		 * @param string $orig The original string.
+		 * @param string $replace The replace string (defaults to a single underscore).
+		 * @return string The new string.
+		 */
+		public static function stoident($original,$replace='_'){
+			return preg_replace('/[^a-zA-Z0-9_]/', $replace, $original);
+		}
+		/**
 		 * Removes special characters from a file name, such as quotes, newlines, path delimiters...
 		 * @param string $filename The file name to check/fix.
 		 * @param string $replaceChar (Optional) Replacement character to be used when a prohibited character is found (default it is empty).
 		 * @param array $ignoreChars (Optional) An array of characters to ignore (let pass through).
 		 * @return string The safe filename (with special characters stripped out/replaced).
+		 * @todo Clean this up.
 		 */
 		public static function filename($filename,$replaceChar='',$ignoreChars=array()){
-			$newname=''; if(is_string($ignoreChars))$ignoreChars=array($ignoreChars);
-			for($i=0; $i<strlen($filename); $i++){
-				if( ((ord($filename{$i})>=48) && (ord($filename{$i})<=57))					// numbers
-				 || ((ord($filename{$i})>=97) && (ord($filename{$i})<=122))					// lowercase
-				 || ((ord($filename{$i})>=65) && (ord($filename{$i})<=90))					// uppercase
-				 || ($filename{$i}=='_') || ($filename{$i}=='-') || ($filename{$i}==' ')	// underscore/dash/space
-				 || ($filename{$i}=='(') || ($filename{$i}==')')							// round brackets
-				 || ($filename{$i}=='[') || ($filename{$i}==']')							// square brackets
-				 || ($filename{$i}=='{') || ($filename{$i}=='}')							// curly brackets
-				 || in_array($filename{$i}, $ignoreChars)									// ignorable characters
-				){ $newname.=$filename{$i}; }else{ $newname.=$replaceChar; }
-			}
-			return $newname;
+			$ignoreChars = preg_quote(implode('',$ignoreChars),'/');
+			$pattern = '/[^a-zA-Z0-9-_ \(\)\[\]\{\}'.$ignoreChars.']/';
+			return preg_replace($pattern, $replaceChar, $filename);
 		}
 	}
 	
